fa544b784dedb3e6d89f722785f7c1a51c6803ea
bougzy <yubeespecial@gmail.com>
2024-08-10 22:12:20 +0100

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Image, Modal } from 'react-bootstrap';
-import Slide26 from '../assets/Slide26.png';
-import Slide23 from '../assets/Slide23.png';
-import Slide24 from '../assets/Slide24.png';
-import Slide25 from '../assets/Slide25.png';
-import Slide20 from '../assets/Slide20.png';
-import Slide21 from '../assets/Slide21.png';
+import slidesix from '../assets/slidesix.png';
+import slitree from '../assets/slitree.png';
+import slifor from '../assets/slifor.png';
+import slifive from '../assets/slifive.png';
+import slio from '../assets/slio.png';
+import slione from '../assets/slione.png';
 
 const ImageGallery = () => {
   const images = [
-    { id: 1, src: Slide26, alt: 'Vision Image' },
-    { id: 2, src: Slide23, alt: 'Hall of Fame Image' },
-    { id: 3, src: Slide24, alt: 'Hat Image' },
-    { id: 4, src: Slide25, alt: 'Placeholder Image' },
-    { id: 5, src: Slide20, alt: 'Placeholder Image' },
-    { id: 6, src: Slide21, alt: 'Placeholder Image' }
+    { id: 1, src: slidesix, alt: 'Vision Image' },
+    { id: 2, src: slitree, alt: 'Hall of Fame Image' },
+    { id: 3, src: slifor, alt: 'Hat Image' },
+    { id: 4, src: slifive, alt: 'Placeholder Image' },
+    { id: 5, src: slio, alt: 'Placeholder Image' },
+    { id: 6, src: slione, alt: 'Placeholder Image' }
   ];
 
   const [show, setShow] = useState(false);
